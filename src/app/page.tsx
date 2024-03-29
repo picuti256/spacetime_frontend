c9a22fc60fa543148a27bf96ba55e3609e35e018
Giovanne Piçuti <picuti256@gmail.com>
2024-03-29 11:58:28 -0300

    Add logos and backgrounds

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,39 @@
+import { User } from "lucide-react";
+import Image from "next/image";
+import logo from "../assets/logo-spacetime.svg";
+
 export default function Home() {
   return (
     <main className="grid grid-cols-2 min-h-screen">
       {/* Left */}
-      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden">
+      <div className="flex flex-col items-start justify-between px-28 py-16 relative overflow-hidden  border-r border-white/10 bg-[url(../assets/bg-stars.svg)] bg-cover">
+        {/* Blur */}
         <div className="absolute right-0 top-1/2 h-[288px] w-[526px] bg-purple-700 opacity-50 -translate-y-1/2 translate-x-1/2 rounded-full blur-full" />
+        {/* Stripes */}
+        <div className="absolute right-2 top-0 bottom-0 w-2 bg-stripes" />
+
+        {/* Sign In */}
+        <a
+          href="/"
+          className="flex items-center gap-3 text-left hover:text-gray-50 transition-colors"
+        >
+          <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+            <User className="h-5 w-5 text-gray-500" />
+          </div>
+          <p className="text-sm leading-snug max-w-[140px]">
+            <span className="underline">Crie sua conta</span> e salve suas
+            memórias!
+          </p>
+        </a>
+
+        {/* Hero */}
+        <div className="">
+          <Image src={logo} alt="Logo spacetime." />
+        </div>
       </div>
 
       {/* Right */}
-      <div className="flex flex-col p-16">
+      <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
         <div className="flex flex-1 items-center justify-center">
           <p className="text-center leading-relaxed w-[360px]">
             Você ainda não registrou nenhuma lembrança, comece a{" "}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -57,6 +57,9 @@ const config: Config = {
         stripes:
           "linear-gradient(to bottom, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.1) 12.5%, transparent 12.5%, transparent)",
       },
+      backgroundSize: {
+        stripes: '100% 8px'
+      }
     },
   },
   plugins: [],
